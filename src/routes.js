7c05d96b367b12a15a5757baf74fabb836237300
EddWebPro <eduardihtiarov408@gmail.com>
2024-07-25 17:42:19 +0500

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
+
+const routerHistory = createWebHashHistory()
+
+import HomePage from '@/pages/home.vue'
+import AboutPage from '@/pages/about.vue'
+import NotFoundPage from '@/pages/notFound.vue'
+
+
+const routers = createRouter({
+    history: routerHistory,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutPage,
+        },
+        {
+            path: '/:CatchAll(.*)',
+            name: '404',
+            component: NotFoundPage,
+        },
+
+        // {
+        //     path: '/:PathMatch(.*)*',
+        //     component: <h1 class='title'>404 Not Found</h1>,
+        // },
+    ]
+})
+
+export default routers;
